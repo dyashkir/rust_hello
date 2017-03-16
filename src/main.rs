4ff966a917d2ec904e767dfaf965ff89d6fd86b8
Dmytro Yashkir <dima.yashkir@gmail.com>
2017-03-16 17:32:24 +0900

    csv parsing of minst

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,55 @@
+/*extern crate rand;
+
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+*/
+
+extern crate csv;
+use std::io;
+use std::io::prelude::*;
+use std::fs::File;
+
 fn main() {
-    println!("Hello, world!");
+/*
+    println!("Guess the number!");
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    //println!("The secret number is: {}", secret_number);
+
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less    => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal   => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+    */
+
+
+    let mut rdr = csv::Reader::from_file("./data/mnist_train.csv").unwrap();
+    
+    for record in rdr.decode() {
+        let mut v = vec![0.0; 1000];
+        v = record.unwrap();
+        //println!("{:?}", v);
+    }
+
+    
 }
